8567d35cacdcfea37537b0e235802376fc69e882
Aaron Faanes <dafrito@gmail.com>
2012-06-12 04:04:19 -0500

    whitespace

--- lib/LuaGlobal.hpp
+++ lib/LuaGlobal.hpp
@@ -6,30 +6,30 @@
 
 class LuaGlobal : public LuaValue
 {
-	const QString key;
+    const QString key;
 protected:
-	void push(LuaStack& stack) const
-	{
-		stack.global(key);
-	}
+    void push(LuaStack& stack) const
+    {
+        stack.global(key);
+    }
 public:
-	LuaGlobal(Lua& lua, const QString& key) :
-		LuaValue(lua), key(key) {}
+    LuaGlobal(Lua& lua, const QString& key) :
+        LuaValue(lua), key(key) {}
 
-	template<typename T>
-	const LuaValue& operator=(const T& value)
-	{
-		LuaStack(lua).setGlobal(key, value);
-		return *this;
-	}
+    template<typename T>
+    const LuaValue& operator=(const T& value)
+    {
+        LuaStack(lua).setGlobal(key, value);
+        return *this;
+    }
 
-	template <typename... Args>
-	LuaReference operator()(Args... args)
-	{
-		LuaStack stack(lua);
-		push(stack);
-		return callLua(luaState(), stack, args...);
-	}
+    template <typename... Args>
+    LuaReference operator()(Args... args)
+    {
+        LuaStack stack(lua);
+        push(stack);
+        return callLua(luaState(), stack, args...);
+    }
 };
 
 #endif // LUAGLOBAL_HPP
