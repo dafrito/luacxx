b124c67f6d781426818dffe70c1442fbcbc6cecb
Aaron Faanes <dafrito@gmail.com>
2012-06-12 03:39:21 -0500

    Converted LuaReference.hpp to spaces

--- lib/LuaReference.hpp
+++ lib/LuaReference.hpp
@@ -7,73 +7,73 @@
 
 class LuaReference : public LuaValue
 {
-	int ref;
+    int ref;
 protected:
-	void push(LuaStack&) const
-	{
-		lua_rawgeti(luaState(), LUA_REGISTRYINDEX, ref);
-	}
+    void push(LuaStack&) const
+    {
+        lua_rawgeti(luaState(), LUA_REGISTRYINDEX, ref);
+    }
 public:
-	LuaReference(Lua& lua) : LuaValue(lua)
-	{
-		ref = luaL_ref(luaState(), LUA_REGISTRYINDEX);
-	}
+    LuaReference(Lua& lua) : LuaValue(lua)
+    {
+        ref = luaL_ref(luaState(), LUA_REGISTRYINDEX);
+    }
 
-	~LuaReference()
-	{
-		luaL_unref(luaState(), LUA_REGISTRYINDEX, ref);
-	}
+    ~LuaReference()
+    {
+        luaL_unref(luaState(), LUA_REGISTRYINDEX, ref);
+    }
 
-	template<typename T>
-	const LuaReference& operator=(const T& value)
-	{
-		LuaStack s(lua);
-		s.push(value);
-		lua_rawseti(luaState(), LUA_REGISTRYINDEX, ref);
-		return *this;
-	}
+    template<typename T>
+    const LuaReference& operator=(const T& value)
+    {
+        LuaStack s(lua);
+        s.push(value);
+        lua_rawseti(luaState(), LUA_REGISTRYINDEX, ref);
+        return *this;
+    }
 
-	template <typename... Args>
-	LuaReference operator()(Args... args)
-	{
-		LuaStack stack(lua);
-		push(stack);
-		return callLua(luaState(), stack, args...);
-	}
+    template <typename... Args>
+    LuaReference operator()(Args... args)
+    {
+        LuaStack stack(lua);
+        push(stack);
+        return callLua(luaState(), stack, args...);
+    }
 };
 
 namespace
 {
-	template <unsigned L, unsigned I>
-	struct Pusher
-	{
-		template <typename Tuple>
-		static void push(LuaStack& stack, Tuple& tuple)
-		{
-			stack.push(std::get<I>(tuple));
-			Pusher<L-1, I+1>::push(stack, tuple);
-		}
-	};
+    template <unsigned L, unsigned I>
+    struct Pusher
+    {
+        template <typename Tuple>
+        static void push(LuaStack& stack, Tuple& tuple)
+        {
+            stack.push(std::get<I>(tuple));
+            Pusher<L-1, I+1>::push(stack, tuple);
+        }
+    };
 
-	template <unsigned I>
-	struct Pusher<0, I>
-	{
-		template <typename Tuple>
-		static void push(LuaStack&, Tuple&)
-		{
-		}
-	};
+    template <unsigned I>
+    struct Pusher<0, I>
+    {
+        template <typename Tuple>
+        static void push(LuaStack&, Tuple&)
+        {
+        }
+    };
 }
 
 template <typename... Args>
 LuaReference callLua(lua_State* s, LuaStack& stack, Args... args)
 {
-	typedef std::tuple<Args...> ArgTuple;
-	ArgTuple argTuple(args...);
-	Pusher<sizeof...(Args), 0>::push(stack, argTuple);
-	// Call Lua function.
-	lua_call(s, sizeof...(Args), 1);
-	return stack.save();
+    typedef std::tuple<Args...> ArgTuple;
+    ArgTuple argTuple(args...);
+    Pusher<sizeof...(Args), 0>::push(stack, argTuple);
+    // Call Lua function.
+    lua_call(s, sizeof...(Args), 1);
+    return stack.save();
 }
 
 #endif // LUAREFERENCE_HPP
