e9e949391236121a44ed8a5e150d3b78a41232e0
Aaron Faanes <dafrito@gmail.com>
2013-05-21 23:37:51 -0500

    Ensure nil references do not cause corruption
    Lua's referenc implementation is very weak. If we store a nil value in a
    reference, other references can obtain the reference id (represented
    here by ref) and overwrite that value. This leads to corruption.
    
    The solution here is still slightly weak. I use a numeric value to
    represent nil values, as well as monitoring the type to ensure we don't
    erroneously treat a real numeric value as nil. I believe the type()
    checks will ensure we never treat a nil value as an integer or treat an
    integer as a nil value.
    
    The highest level of protection would be to use a table or function to
    represent the nil, but there's no easy way to save that value in the
    environment. This is because this class is so fundamental that it can't
    use the LuaEnvironment or LuaStack.
    
    Unfortunately, this means that we're stuck with this complexity.

--- include/stack/reference.cpp
+++ include/stack/reference.cpp
@@ -3,8 +3,10 @@ class LuaReferenceAccessible : public LuaAccessible
 
     class RawLuaReference
     {
+        constexpr static int NIL_VALUE = 0xdead;
+
         int ref;
-        mutable int lastType;
+        mutable int _type;
         lua_State* const _state;
 
         lua_State* luaState() const
@@ -17,14 +19,12 @@ class LuaReferenceAccessible : public LuaAccessible
             _state(state)
         {
             auto before = lua_gettop(luaState());
-            lua_pushinteger(luaState(), 0);
-            ref = luaL_ref(luaState(), LUA_REGISTRYINDEX);
-            lastType = type();
 
-            // Push an integer first, then set to nil to prevent
-            // Lua from returning LUA_REFNIL for nil values.
-            lua_pushnil(luaState());
-            store();
+            lua_pushinteger(luaState(), NIL_VALUE);
+            ref = luaL_ref(luaState(), LUA_REGISTRYINDEX);
+            _type = LUA_TNIL;
+            assert(isNil());
+
             auto after = lua_gettop(luaState());
             assert(before == after);
         }
@@ -33,20 +33,50 @@ class LuaReferenceAccessible : public LuaAccessible
         {
             lua_rawgeti(luaState(), LUA_REGISTRYINDEX, ref);
             auto currentType = lua_type(luaState(), -1);
+            if (currentType == LUA_TNUMBER && lua_tonumber(luaState(), -1) == NIL_VALUE && _type == LUA_TNIL) {
+                currentType = LUA_TNIL;
+            }
             lua_pop(luaState(), 1);
             return currentType;
         }
 
+        bool isNil() const
+        {
+            return type() == LUA_TNIL;
+        }
+
+
         void push() const
         {
-            assert(lastType == type());
+            assert(_type == type());
+            if (isNil()) {
+                lua_pushnil(luaState());
+                return;
+            }
             lua_rawgeti(luaState(), LUA_REGISTRYINDEX, ref);
+        }
+
+        void storeNil() const
+        {
+            lua_pushinteger(luaState(), NIL_VALUE);
+            lua_rawseti(luaState(), LUA_REGISTRYINDEX, ref);
+            _type = LUA_TNIL;
+            assert(isNil());
         }
 
         void store() const
         {
+            if (lua_type(luaState(), -1) == LUA_TNIL) {
+                storeNil();
+
+                // Pop the nil value that was on the stack
+                lua_pop(luaState(), 1);
+
+                return;
+            }
+
             lua_rawseti(luaState(), LUA_REGISTRYINDEX, ref);
-            lastType = type();
+            _type = type();
         }
 
         ~RawLuaReference()
