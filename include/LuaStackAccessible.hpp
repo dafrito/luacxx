cc0de0f23669a45a944638852473724520d48bf1
Aaron Faanes <dafrito@gmail.com>
2013-10-16 10:01:24 -0500

    LuaStackAccessible now uses a global position, rather than stack-relative

--- include/LuaStackAccessible.hpp
+++ include/LuaStackAccessible.hpp
@@ -9,25 +9,50 @@ class LuaStackAccessible : public LuaAccessible
 {
     const int _pos;
 public:
+    LuaStackAccessible(LuaStack& stack, const int pos) :
+        _pos(stack.absPos(pos))
+    {
+    }
+
     LuaStackAccessible(const int pos) :
         _pos(pos)
-    {}
+    {
+        if (pos == 0) {
+            throw std::out_of_range("Stack position must not be zero");
+        }
+    }
 
     int pos() const
     {
         return _pos;
     }
 
+    int absPos(LuaStack& stack) const
+    {
+        auto index = pos();
+        if (index < 0) {
+            index += lua_gettop(stack.luaState()) + 1;
+        }
+        if (index <= 0) {
+            throw std::out_of_range("Stack position must not underflow");
+        }
+        if (index > lua_gettop(stack.luaState())) {
+            throw std::out_of_range("Stack position must not overflow");
+        }
+        return index;
+    }
+
     void push(LuaStack& stack) const
     {
-        stack.pushCopy(pos());
+        // Use the Lua API since the position is treated as an absolute index
+        lua_pushvalue(stack.luaState(), absPos(stack));
     }
 
     void store(LuaStack& stack) const
     {
         // Push a copy of the top value so that it's unaffected by this operation
-        stack.pushCopy();
-        stack.replace(pos());
+        lua_pushvalue(stack.luaState(), -1);
+        lua_replace(stack.luaState(), absPos(stack));
     }
 };
 
