04492482fb07124fe67fd5c5d20c39029abfb4a7
Aaron Faanes <dafrito@gmail.com>
2014-05-13 00:37:40 -0500

    Separate lua::index into its own header

--- src/push.hpp
+++ src/push.hpp
@@ -6,6 +6,7 @@
 
 #include "type.hpp"
 #include "state.hpp"
+#include "index.hpp"
 
 namespace lua {
 
@@ -68,95 +69,6 @@ struct Push
     static void push(lua::state* const state, T orig)
     {
         push_userdata(state, orig);
-    }
-};
-
-class index
-{
-    lua::state* _state;
-    int _pos;
-
-public:
-    index(lua::state* const state, const int pos) :
-        _state(state),
-        _pos(lua_absindex(state, pos))
-    {
-    }
-
-    index(const lua::index& other) :
-        _state(other._state),
-        _pos(other._pos)
-    {
-    }
-
-    lua::state* const state() const
-    {
-        return _state;
-    }
-
-    int pos() const
-    {
-        return _pos;
-    }
-
-    void set(const int pos)
-    {
-        _pos = pos;
-    }
-
-    void set(const index& other)
-    {
-        _pos = other.pos();
-    }
-
-    lua::type_info type() const
-    {
-        return lua::type_info(
-            static_cast<lua::type>(lua_type(state(), pos()))
-        );
-    }
-
-    explicit operator bool() const
-    {
-        return _pos >= 1 && _pos <= lua_gettop(_state);
-    }
-
-    template <class T>
-    const index& operator=(T source) const
-    {
-        lua::Push<T>::push(state(), source);
-        lua_replace(state(), pos());
-        return *this;
-    }
-
-    // Postfix (e.g. i++)
-    lua::index operator++(int)
-    {
-        lua::index rv(*this);
-        ++(*this);
-        return rv;
-    }
-
-    // Prefix (e.g. ++i)
-    lua::index& operator++()
-    {
-        ++_pos;
-        return *this;
-    }
-
-    // Postfix (e.g. i--)
-    lua::index operator--(int)
-    {
-        lua::index rv(*this);
-        --(*this);
-        return rv;
-    }
-
-    // Prefix (e.g. --i)
-    lua::index& operator--()
-    {
-        --_pos;
-        return *this;
     }
 };
 
