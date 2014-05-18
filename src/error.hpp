b0ed74f955c3d7dfedfb8a67a1e51d32c54577ab
Aaron Faanes <dafrito@gmail.com>
2014-05-18 02:14:21 -0500

    Don't use the c_str from the temporary std::string returned from what

--- src/error.hpp
+++ src/error.hpp
@@ -9,32 +9,48 @@ namespace lua {
 class error : public std::runtime_error
 {
     std::string _what;
-    std::string _traceback;
+    int _traceback;
 
 public:
-    error(const std::string& reason) :
+    error(const std::string& what) :
         std::runtime_error("lua::error"),
-        _what(reason),
-        _traceback()
+        _what(what),
+        _traceback(what.size())
     {
     }
 
-    const char* what() const noexcept
+    error() :
+        std::runtime_error("lua::error"),
+        _what("An unspecified Lua error occurred."),
+        _traceback(_what.size())
     {
-        if (!traceback().empty()) {
-            return (_what + "\n" + traceback()).c_str();
-        }
+    }
+
+    virtual const char* what() const noexcept override
+    {
         return _what.c_str();
     }
 
-    const std::string& traceback() const
+    const bool has_traceback() const
     {
-        return _traceback;
+        return _what.size() <= _traceback;
+    }
+
+    const std::string traceback() const
+    {
+        if (!has_traceback()) {
+            return "";
+        }
+        return _what.substr(_traceback + 1);
     }
 
     void set_traceback(const std::string& traceback)
     {
-        _traceback = traceback;
+        if (!has_traceback()) {
+            _what += "\n" + traceback;
+        } else {
+            _what = _what.substr(0, _traceback) + "\n" + traceback;
+        }
     }
 };
 
