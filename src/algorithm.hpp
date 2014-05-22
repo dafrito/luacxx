a53d1a58062199091135879a15a885a95e054dc6
Aaron Faanes <dafrito@gmail.com>
2014-05-22 02:18:25 -0500

    minor debug

--- src/algorithm.hpp
+++ src/algorithm.hpp
@@ -9,8 +9,6 @@
 #include <vector>
 #include <sstream>
 
-#include <iostream>
-
 namespace lua {
 
 std::string traceback(lua::state* const state, const int toplevel);
@@ -117,7 +115,6 @@ void insert(Table destination, Value value)
     lua::assert_type("lua::table::insert", lua::type::table, table);
     lua::push(table.state(), lua::table::length(table) + 1);
     lua::push(table.state(), value);
-    std::cerr << lua::dump(table.state()) << std::endl;
 
     lua_settable(table.state(), table.pos());
 
