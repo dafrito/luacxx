71ae68508b9c50967f22af7eb619d4b23671056c
Aaron Faanes <dafrito@gmail.com>
2014-09-29 01:35:16 -0500

    Completed QActionGroup binding

--- src/Qt5Widgets/QActionGroup.cpp
+++ src/Qt5Widgets/QActionGroup.cpp
@@ -4,18 +4,56 @@
 
 #include <QActionGroup>
 
+#include "../Qt5Core/QList.hpp"
+#include "../Qt5Core/QAction.hpp"
+#include "../Qt5Core/QString.hpp"
+#include "../Qt5Gui/QIcon.hpp"
+#include "../Qt5Core/QObject.hpp"
+
 // http://qt-project.org/doc/qt-5/qactiongroup.html
+
+int QActionGroup_addAction(lua_State* const state)
+{
+    auto self = lua::get<QActionGroup*>(state, 1);
+
+    if (lua::size(state) > 2) {
+        lua::push(state, self->addAction(
+            lua::get<const QIcon&>(state, 2),
+            lua::get<const QString&>(state, 3)
+        ));
+        return 1;
+    }
+
+    if (lua::is_type<QAction>(state, 2)) {
+        lua::push(state, self->addAction(
+            lua::get<QAction*>(state, 2)
+        ));
+        return 1;
+    }
+
+    lua::push(state, self->addAction(
+        lua::get<const QString&>(state, 2)
+    ));
+    return 1;
+}
 
 void lua::QActionGroup_metatable(const lua::index& mt)
 {
-    // TODO Set up metatable methods for this class
+    lua::QObject_metatable(mt);
+
+    mt["actions"] = &QActionGroup::actions;
+    mt["addAction"] = QActionGroup_addAction;
+    mt["checkedAction"] = &QActionGroup::checkedAction;
+    mt["isEnabled"] = &QActionGroup::isEnabled;
+    mt["isExclusive"] = &QActionGroup::isExclusive;
+    mt["isVisible"] = &QActionGroup::isVisible;
+    mt["removeAction"] = &QActionGroup::removeAction;
 }
 
 int QActionGroup_new(lua_State* const state)
 {
-    lua::make<QActionGroup>(state, nullptr);
-    // TODO Set up object-specific methods
-
+    // QActionGroup(QObject * parent)
+    lua::make<QActionGroup>(state, lua::get<QObject*>(state, 1));
     return 1;
 }
 
