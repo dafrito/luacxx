b08282916fbe7115a1eaed313f9910b5546bb04e
Aaron Faanes <dafrito@gmail.com>
2014-10-23 00:42:03 -0500

    Add Qt5Widgets tests

---
+++ src/tests/Qt5Widgets.cpp
@@ -0,0 +1,62 @@
+#define BOOST_TEST_MODULE Qt5Widgets
+#include "Qt5Widgets.hpp"
+
+#include <boost/test/unit_test.hpp>
+
+#include "thread.hpp"
+#include "stack.hpp"
+#include "algorithm.hpp"
+#include "load.hpp"
+#include "reference.hpp"
+
+#include "convert/string.hpp"
+#include "convert/char.hpp"
+#include "convert/callable.hpp"
+#include "convert/numeric.hpp"
+
+#include "Qt5Core/QString.hpp"
+#include "Qt5Core/QChar.hpp"
+#include "Qt5Core/QVariant.hpp"
+#include "Qt5Core/QObject.hpp"
+
+#include "load/DirectoryModuleLoader.hpp"
+#include <QDir>
+#include <QPoint>
+
+#include <QApplication>
+
+struct QApplicationFixture
+{
+    int argc;
+    char name[100];
+    char* argv[1];
+
+    QApplication* app;
+public:
+    QApplicationFixture() :
+        argc(1)
+    {
+        strcpy(name, "luacxx");
+        argv[0] = name;
+        app = new QApplication(argc, argv);
+    }
+
+    ~QApplicationFixture()
+    {
+        delete app;
+    }
+};
+
+BOOST_GLOBAL_FIXTURE(QApplicationFixture);
+
+BOOST_AUTO_TEST_CASE(Qt5Widgets_QGraphicsScene)
+{
+    auto env = lua::create();
+
+    BOOST_REQUIRE(qApp != nullptr);
+    env["package"]["cpath"] = ".libs/libluacxx-?.so";
+
+    lua::run_string(env, "require 'Qt5Widgets.QGraphicsScene'");
+    lua::run_string(env, "require 'Qt5Widgets.QGraphicsObject'");
+    lua::run_string(env, "foo = QGraphicsScene.new()");
+}
---
+++ src/tests/Qt5Widgets.hpp
@@ -0,0 +1,75 @@
+#ifndef LUACXX_tests_Qt5Gui_INCLUDED
+#define LUACXX_tests_Qt5Gui_INCLUDED
+
+#include "Qt5Core/QObject.hpp"
+
+#include <QObject>
+#include <QPoint>
+
+class QtPoint : public QObject
+{
+    Q_OBJECT
+
+    int _x;
+    int _y;
+
+    Q_PROPERTY(int x READ getX WRITE setX)
+    Q_PROPERTY(int y READ getY WRITE setY)
+public:
+
+    QtPoint() :
+        _x(),
+        _y()
+    {
+    }
+
+    QtPoint(const int x, const int y) :
+        _x(x),
+        _y(y)
+    {
+    }
+
+    Q_INVOKABLE void setX(const int x)
+    {
+        _x = x;
+        emit xChanged();
+    }
+
+    Q_INVOKABLE const int getX() const
+    {
+        return _x;
+    }
+
+    Q_INVOKABLE void setY(const int y)
+    {
+        _y = y;
+        emit yChanged();
+    }
+
+    Q_INVOKABLE const int getY() const
+    {
+        return _y;
+    }
+
+signals:
+    void xChanged() const;
+    void yChanged() const;
+};
+
+namespace lua {
+
+template <>
+struct Metatable<QtPoint>
+{
+    static constexpr const char* name = "QtPoint";
+
+    static bool metatable(lua_State* const state, const int mt, const QtPoint* const)
+    {
+        lua::QObject_metatable(state, mt);
+        return true;
+    }
+};
+
+}; // namespace lua
+
+#endif // LUACXX_tests_Qt5Gui_INCLUDED
