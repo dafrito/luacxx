32cb74d54101c6a3635248839d00e998ba57b97d
Aaron Faanes <dafrito@gmail.com>
2014-11-08 17:04:36 -0600

    Added comments and definitions to QGraphicsItem

--- src/Qt5Widgets/QGraphicsItem.cpp
+++ src/Qt5Widgets/QGraphicsItem.cpp
@@ -21,120 +21,241 @@
 #include "QGraphicsItemGroup.hpp"
 #include "../Qt5Core/QVariant.hpp"
 
+#include <iostream>
+
 int QGraphicsItem_collidesWithItem(lua_State* const state)
 {
+    // virtual bool     collidesWithItem(const QGraphicsItem * other, Qt::ItemSelectionMode mode = Qt::IntersectsItemShape) const
+    std::cerr << "QGraphicsItem_collidesWithItem\n";
     return 0;
 }
 int QGraphicsItem_collidesWithPath(lua_State* const state)
 {
-    return 0;
-}
-int QGraphicsItem_cursor(lua_State* const state)
-{
+    // virtual bool     collidesWithPath(const QPainterPath & path, Qt::ItemSelectionMode mode = Qt::IntersectsItemShape) const
+    std::cerr << "QGraphicsItem_collidesWithPath\n";
     return 0;
 }
 int QGraphicsItem_collidingItems(lua_State* const state)
 {
+    // QList<QGraphicsItem *>   collidingItems(Qt::ItemSelectionMode mode = Qt::IntersectsItemShape) const
+    std::cerr << "QGraphicsItem_collidingItems\n";
     return 0;
 }
 int QGraphicsItem_ensureVisible(lua_State* const state)
 {
+    //  void    ensureVisible(const QRectF & rect = QRectF(), int xmargin = 50, int ymargin = 50)
+    //  void    ensureVisible(qreal x, qreal y, qreal w, qreal h, int xmargin = 50, int ymargin = 50)
+    std::cerr << "QGraphicsItem_ensureVisible\n";
     return 0;
 }
 int QGraphicsItem_isObscured(lua_State* const state)
 {
+    // bool     isObscured(const QRectF & rect = QRectF()) const
+    // bool     isObscured(qreal x, qreal y, qreal w, qreal h) const
+    std::cerr << "QGraphicsItem_isObscured\n";
     return 0;
 }
 int QGraphicsItem_itemTransform(lua_State* const state)
 {
+    // QTransform   itemTransform(const QGraphicsItem * other, bool * ok = 0) const
+    std::cerr << "QGraphicsItem_itemTransform\n";
     return 0;
 }
 int QGraphicsItem_mapFromItem(lua_State* const state)
 {
+    // QPointF  mapFromItem(const QGraphicsItem * item, const QPointF & point) const
+    // QPolygonF    mapFromItem(const QGraphicsItem * item, const QRectF & rect) const
+    // QPolygonF    mapFromItem(const QGraphicsItem * item, const QPolygonF & polygon) const
+    // QPainterPath     mapFromItem(const QGraphicsItem * item, const QPainterPath & path) const
+    // QPolygonF    mapFromItem(const QGraphicsItem * item, qreal x, qreal y, qreal w, qreal h) const
+    // QPointF  mapFromItem(const QGraphicsItem * item, qreal x, qreal y) const
+    std::cerr << "QGraphicsItem_mapFromItem\n";
     return 0;
 }
 int QGraphicsItem_mapFromParent(lua_State* const state)
 {
+    // QPointF  mapFromParent(const QPointF & point) const
+    // QPolygonF    mapFromParent(const QRectF & rect) const
+    // QPolygonF    mapFromParent(const QPolygonF & polygon) const
+    // QPainterPath     mapFromParent(const QPainterPath & path) const
+    // QPolygonF    mapFromParent(qreal x, qreal y, qreal w, qreal h) const
+    // QPointF  mapFromParent(qreal x, qreal y) const
+    std::cerr << "QGraphicsItem_mapFromParent\n";
     return 0;
 }
 int QGraphicsItem_mapFromScene(lua_State* const state)
 {
+    // QPointF  mapFromScene(const QPointF & point) const
+    // QPolygonF    mapFromScene(const QRectF & rect) const
+    // QPolygonF    mapFromScene(const QPolygonF & polygon) const
+    // QPainterPath     mapFromScene(const QPainterPath & path) const
+    // QPolygonF    mapFromScene(qreal x, qreal y, qreal w, qreal h) const
+    // QPointF  mapFromScene(qreal x, qreal y) const
+    std::cerr << "QGraphicsItem_mapFromScene\n";
     return 0;
 }
 int QGraphicsItem_mapRectFromItem(lua_State* const state)
 {
+    // QRectF   mapRectFromItem(const QGraphicsItem * item, const QRectF & rect) const
+    // QRectF   mapRectFromItem(const QGraphicsItem * item, qreal x, qreal y, qreal w, qreal h) const
+    std::cerr << "QGraphicsItem_mapRectFromItem\n";
     return 0;
 }
 int QGraphicsItem_mapRectFromParent(lua_State* const state)
 {
+    // QRectF   mapRectFromParent(const QRectF & rect) const
+    // QRectF   mapRectFromParent(qreal x, qreal y, qreal w, qreal h) const
+    std::cerr << "QGraphicsItem_mapRectFromParent\n";
     return 0;
 }
 int QGraphicsItem_mapRectFromScene(lua_State* const state)
 {
+    // QRectF   mapRectFromScene(const QRectF & rect) const
+    // QRectF   mapRectFromScene(qreal x, qreal y, qreal w, qreal h) const
+    std::cerr << "QGraphicsItem_mapRectFromScene\n";
     return 0;
 }
 int QGraphicsItem_mapRectToItem(lua_State* const state)
 {
+    //  QRectF  mapRectToItem(const QGraphicsItem * item, const QRectF & rect) const
+    //  QRectF  mapRectToItem(const QGraphicsItem * item, qreal x, qreal y, qreal w, qreal h) const
+    std::cerr << "QGraphicsItem_mapRectToItem\n";
     return 0;
 }
 int QGraphicsItem_mapRectToParent(lua_State* const state)
 {
+    // QRectF   mapRectToParent(const QRectF & rect) const
+    // QRectF   mapRectToParent(qreal x, qreal y, qreal w, qreal h) const
+    std::cerr << "QGraphicsItem_mapRectToParent\n";
     return 0;
 }
 int QGraphicsItem_mapRectToScene(lua_State* const state)
 {
+    // QRectF   mapRectToScene(const QRectF & rect) const
+    // QRectF   mapRectToScene(qreal x, qreal y, qreal w, qreal h) const
+    std::cerr << "QGraphicsItem_mapRectToScene\n";
     return 0;
 }
 int QGraphicsItem_mapToItem(lua_State* const state)
 {
+    // QPointF  mapToItem(const QGraphicsItem * item, const QPointF & point) const
+    // QPolygonF    mapToItem(const QGraphicsItem * item, const QRectF & rect) const
+    // QPolygonF    mapToItem(const QGraphicsItem * item, const QPolygonF & polygon) const
+    // QPainterPath     mapToItem(const QGraphicsItem * item, const QPainterPath & path) const
+    // QPolygonF    mapToItem(const QGraphicsItem * item, qreal x, qreal y, qreal w, qreal h) const
+    // QPointF  mapToItem(const QGraphicsItem * item, qreal x, qreal y) const
+    std::cerr << "QGraphicsItem_mapToItem\n";
     return 0;
 }
 int QGraphicsItem_mapToParent(lua_State* const state)
 {
+    //  QPointF     mapToParent(const QPointF & point) const
+    //  QPolygonF   mapToParent(const QRectF & rect) const
+    //  QPolygonF   mapToParent(const QPolygonF & polygon) const
+    //  QPainterPath    mapToParent(const QPainterPath & path) const
+    //  QPolygonF   mapToParent(qreal x, qreal y, qreal w, qreal h) const
+    //  QPointF     mapToParent(qreal x, qreal y) const
+    std::cerr << "QGraphicsItem_mapToParent\n";
     return 0;
 }
 int QGraphicsItem_mapToScene(lua_State* const state)
 {
+    // QPointF  mapToScene(const QPointF & point) const
+    // QPolygonF    mapToScene(const QRectF & rect) const
+    // QPolygonF    mapToScene(const QPolygonF & polygon) const
+    // QPainterPath     mapToScene(const QPainterPath & path) const
+    // QPolygonF    mapToScene(qreal x, qreal y, qreal w, qreal h) const
+    // QPointF  mapToScene(qreal x, qreal y) const
+    std::cerr << "QGraphicsItem_mapToScene\n";
     return 0;
 }
 int QGraphicsItem_paint(lua_State* const state)
 {
+    // virtual void     paint(QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget = 0) = 0
+    std::cerr << "QGraphicsItem_paint\n";
     return 0;
 }
 int QGraphicsItem_scroll(lua_State* const state)
 {
+    // void     scroll(qreal dx, qreal dy, const QRectF & rect = QRectF())
+    std::cerr << "QGraphicsItem_scroll\n";
     return 0;
 }
 int QGraphicsItem_setCacheMode(lua_State* const state)
 {
+    // void     setCacheMode(CacheMode mode, const QSize & logicalCacheSize = QSize())
+    std::cerr << "QGraphicsItem_setCacheMode\n";
     return 0;
 }
 int QGraphicsItem_setFlag(lua_State* const state)
 {
+    // void     setFlag(GraphicsItemFlag flag, bool enabled = true)
+    std::cerr << "QGraphicsItem_setFlag\n";
     return 0;
 }
 int QGraphicsItem_setFocus(lua_State* const state)
 {
+    // void     setFocus(Qt::FocusReason focusReason = Qt::OtherFocusReason)
+    std::cerr << "QGraphicsItem_setFocus\n";
     return 0;
 }
+
 int QGraphicsItem_setPos(lua_State* const state)
 {
+    auto self = lua::get<QGraphicsItem*>(state, 1);
+
+    if (lua_gettop(state) == 2) {
+        self->setPos(
+            lua::get<QPointF>(state, 2)
+        );
+    } else {
+        self->setPos(
+            lua::get<qreal>(state, 2),
+            lua::get<qreal>(state, 3)
+        );
+    }
     return 0;
 }
+
 int QGraphicsItem_setTransform(lua_State* const state)
 {
+    // void     setTransform(const QTransform & matrix, bool combine = false)
+    std::cerr << "QGraphicsItem_setTransform\n";
     return 0;
 }
 int QGraphicsItem_setTransformOriginPoint(lua_State* const state)
 {
+    // void     setTransformOriginPoint(const QPointF & origin)
+    // void     setTransformOriginPoint(qreal x, qreal y)
+    std::cerr << "QGraphicsItem_setTransformOriginPoint\n";
     return 0;
 }
 int QGraphicsItem_toGraphicsObject(lua_State* const state)
 {
+    // QGraphicsObject *    toGraphicsObject()
+    // const QGraphicsObject *  toGraphicsObject() const
+    std::cerr << "QGraphicsItem_toGraphicsObject\n";
     return 0;
 }
 int QGraphicsItem_update(lua_State* const state)
 {
+    auto self = lua::get<QGraphicsItem*>(state, 1);
+
+    if (lua_gettop(state) > 2) {
+        // void   update(qreal x, qreal y, qreal width, qreal height)
+        self->update(
+            lua::get<qreal>(state, 2),
+            lua::get<qreal>(state, 3),
+            lua::get<qreal>(state, 4),
+            lua::get<qreal>(state, 5)
+        );
+    } else if (lua_gettop(state) == 1) {
+        // void   update()
+        self->update();
+    } else {
+        // void   update(const QRectF & rect = QRectF())
+        self->update(lua::get<const QRectF&>(state, 2));
+    }
     return 0;
 }
 
@@ -159,7 +280,7 @@ void lua::QGraphicsItem_metatable(lua_State* const state, const int pos)
     mt["collidingItems"] = QGraphicsItem_collidingItems;
     mt["commonAncestorItem"] = &QGraphicsItem::commonAncestorItem;
     mt["contains"] = &QGraphicsItem::contains;
-    mt["cursor"] = QGraphicsItem_cursor;
+    mt["cursor"] = &QGraphicsItem::cursor;
     mt["data"] = &QGraphicsItem::data;
     mt["deviceTransform"] = &QGraphicsItem::deviceTransform;
     mt["effectiveOpacity"] = &QGraphicsItem::effectiveOpacity;
