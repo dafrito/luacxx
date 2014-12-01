e1dd1a091db2f092bb1131f80a18bb10e3f82268
Aaron Faanes <dafrito@gmail.com>
2014-11-30 23:55:14 -0600

    Correctly implement QWindow's inheritance

--- src/Qt5Gui/QWindow.hpp
+++ src/Qt5Gui/QWindow.hpp
@@ -7,6 +7,9 @@
 
 #include <QWindow>
 
+#include "QSurface.hpp"
+#include "../Qt5Core/QObject.hpp"
+
 /*
 
 =head1 NAME
@@ -15,7 +18,34 @@ QWindow - http://qt-project.org/doc/qt-5/qwindow.html
 
 */
 
-LUA_METATABLE_BUILT(QWindow);
+namespace lua {
+
+void QWindow_metatable(lua_State* const state, const int mt);
+
+template <>
+struct Metatable<QWindow>
+{
+    static const userdata_type& info()
+    {
+        static userdata_type _info;
+        if (!_info.has_name()) {
+            _info.set_name("QWindow");
+            _info.add_cast<QWindow>();
+            _info.add_cast<QSurface, QWindow>();
+            _info.add_cast<QObject, QWindow>();
+        }
+        return _info;
+    }
+
+    static bool metatable(lua_State* const state, const int mt, void* const)
+    {
+        lua::QWindow_metatable(state, mt);
+        return true;
+    }
+};
+
+}; // namespace lua
+
 LUA_METATABLE_ENUM(QWindow::AncestorMode);
 LUA_METATABLE_ENUM(QWindow::Visibility);
 
